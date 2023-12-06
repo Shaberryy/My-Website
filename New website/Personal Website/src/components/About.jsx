056f91397c2067c4529e3c57d51eb446c0607bf8
shaberry <jaden.png02@gmail.com>
2023-12-06 13:58:04 -0800

    edit

--- New website/Personal Website/src/components/About.jsx
+++ New website/Personal Website/src/components/About.jsx
@@ -3,6 +3,41 @@ export default function About() {
     <>
       <h1>Hali Jaden Rodriguez</h1>
       <h2>Hi askdjaskdnjasdknadkjandasio meow</h2>
+
+      <footer>
+        <h1>My socials</h1>
+        <span>
+          <div class="socialbox">
+            <a href="https://www.linkedin.com/in/hali-rodriguez-882007229/">
+              {" "}
+              <img
+                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png"
+                className="socials"
+              />
+            </a>
+
+            <a href="https://www.instagram.com/ayeitshali/">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
+                className="socials"
+              />
+            </a>
+
+            <a href="https://github.com/Shaberryy">
+              <img
+                src="https://static-00.iconduck.com/assets.00/github-icon-2048x1988-jzvzcf2t.png"
+                className="socials"
+              />
+            </a>
+          </div>
+        </span>
+        <h1 class="return">
+          <a href="contact.html" class="return">
+            {" "}
+            Contact me!
+          </a>
+        </h1>
+      </footer>
     </>
   );
 }
