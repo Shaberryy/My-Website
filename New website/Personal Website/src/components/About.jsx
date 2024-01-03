86d8d39d15aa2c8203a3eaa4de465c725970d805
Hali <128436801+Shaberryy@users.noreply.github.com>
2024-01-02 21:41:47 -0800

    update

--- New website/Personal Website/src/components/About.jsx
+++ New website/Personal Website/src/components/About.jsx
@@ -7,31 +7,35 @@ export default function About() {
       <div>
         <NavBar />
         <body>
-          {" "}
           <br />
           <h1>Hali Jaden Rodriguez</h1>
-          <p>
-            Hi, I`m Hali Rodriguez, a California-based Full Stack Developer with
-            a passion for digital art and 3D animation. My expertise in a range
-            of design software applications, including Maya, Blender, Clip
-            Studio and Zbrush, allows me to bring a creative edge to my
-            development projects.
-            <br />
-            <br /> When I`m not coding, you can find me immersed in my own art
-            projects, where I love to explore the realms of 3D animation and
-            sculpting. I find inspiration in the intricacies of digital art and
-            the captivating world of video games, which fuels my creativity and
-            problem-solving skills. With a keen eye for detail and a knack for
-            working efficiently both individually and within a team, I can
-            thrive in fast-paced environments.
-            <br />
-            <br /> I`m always open to new ideas and approaches, believing that
-            the best solutions often emerge from collaborative and diverse
-            perspectives. Based in the heart of California, I embrace the
-            vibrant tech community and constantly seek opportunities to expand
-            my skill set and contribute to innovative projects that push the
-            boundaries of digital creativity
-          </p>
+
+          <div className="nameAndPhoto">
+            <img className="hali" src="../src/assets/hali.jpg" />
+
+            <p>
+              Hi, I`m Hali Rodriguez, a California-based Full Stack Developer
+              with a passion for digital art and 3D animation. My expertise in a
+              range of design software applications, including Maya, Blender,
+              Clip Studio and Zbrush, allows me to bring a creative edge to my
+              development projects.
+              <br />
+              <br /> When I`m not coding, you can find me immersed in my own art
+              projects, where I love to explore the realms of 3D animation and
+              sculpting. I find inspiration in the intricacies of digital art
+              and the captivating world of video games, which fuels my
+              creativity and problem-solving skills. With a keen eye for detail
+              and a knack for working efficiently both individually and within a
+              team, I can thrive in fast-paced environments.
+              <br />
+              <br /> I`m always open to new ideas and approaches, believing that
+              the best solutions often emerge from collaborative and diverse
+              perspectives. Based in the heart of California, I embrace the
+              vibrant tech community and constantly seek opportunities to expand
+              my skill set and contribute to innovative projects that push the
+              boundaries of digital creativity
+            </p>
+          </div>
         </body>
         <footer>
           {" "}
