e48e18b845d4558ce71e68fa9f0f878c038c6475
Hali <128436801+Shaberryy@users.noreply.github.com>
2024-01-15 02:37:36 -0800

    divider and image sizing

--- New website/Personal Website/src/components/Home.jsx
+++ New website/Personal Website/src/components/Home.jsx
@@ -5,7 +5,9 @@ export default function Home() {
     <div className="home">
       <NavBar />
       <h1>Hali J. Rodriguez</h1>
+      <h3 className="divider">✧･ﾟ: *✧･ﾟ:* ~·͙*̩̩͙˚̩̥̩̥*̩̩̥͙ ✩ *̩̩̥͙˚̩̥̩̥*̩̩͙‧͙~ *:･ﾟ✧*:･ﾟ✧</h3>
       <h2>Developer & 3Dd Artist</h2>
+
       <body>Iasdadsjadlnakdn</body>
     </div>
   );
